35e304a2eb018f76223f0084efdcf1f7a891afd8
ivander27 <timotius.ivander04@gmail.com>
2022-04-09 20:58:34 +0700

    Rename File

--- app/src/main/java/com/example/login/sign_in.kt
+++ app/src/main/java/com/example/login/sign_in.kt
@@ -2,10 +2,12 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class Profile : AppCompatActivity() {
+class sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-    }
-}
+        setContentView(R.layout.activity_sign_in)
+
+            }
+        }
