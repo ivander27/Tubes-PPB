6c888e09f3e4e1cb8e2754a35c1a522fb31c2757
ivander27 <timotius.ivander04@gmail.com>
2022-04-10 16:44:13 +0700

    update activity_sign_in.xml,sign_in.kt,stringxml

--- app/src/main/java/com/example/login/sign_in.kt
+++ app/src/main/java/com/example/login/sign_in.kt
@@ -1,13 +1,33 @@
 package com.example.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 
-class sign_in : AppCompatActivity() {
+class sign_in : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val button_signin: Button = findViewById(R.id.button_signin)
+        val button_Register: Button = findViewById(R.id.button_register)
+
+        button_signin.setOnClickListener(this)
+        button_Register.setOnClickListener(this)
+    }
+       override fun onClick(v: View) {
+           when (v.id) {
+                R.id.button_signin -> {
+                    val moveBack = Intent(this@sign_in, Home::class.java)
+                    startActivity(moveBack)
+                }
+                R.id.button_register -> {
+                    val moveSignUp = Intent(this@sign_in, SignUp::class.java)
+                    startActivity(moveSignUp)
+                }
             }
         }
+    }
+
