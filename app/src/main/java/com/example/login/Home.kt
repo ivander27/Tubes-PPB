041e2afcd6bbbdc1d7e110bc3e80729b821ffd12
Jestr13 <johanesvictor46@gmail.com>
2022-04-11 22:53:03 +0700

    Raw-UI main ya ges yaa
    dalam nama Tuhan Yesus ga eror, AMINNN

--- app/src/main/java/com/example/login/Home.kt
+++ app/src/main/java/com/example/login/Home.kt
@@ -2,8 +2,16 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 
 class Home : AppCompatActivity() {
+
+    private Toolbar main_toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
