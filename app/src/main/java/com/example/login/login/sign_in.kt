d0000a8dceee0add53dae950434b52bc7e297ec2
ivander27 <timotius.ivander04@gmail.com>
2022-04-17 17:10:08 +0700

    update sign in

--- app/src/main/java/com/example/login/login/sign_in.kt
+++ app/src/main/java/com/example/login/login/sign_in.kt
@@ -2,33 +2,50 @@ package com.example.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class sign_in : AppCompatActivity(), View.OnClickListener {
-                override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_sign_in)
-                Toast.makeText(this ,"firebase connection succes",Toast.LENGTH_LONG).show()
-                val button_signin: Button = findViewById(R.id.button_signin)
-                val button_Register: Button = findViewById(R.id.button_register)
+        private lateinit var emailfield: EditText
+        private lateinit var passwordfield: EditText
+        private lateinit var signinbutton: Button
+        private lateinit var registerbutton: Button
 
-                button_signin.setOnClickListener(this)
-                button_Register.setOnClickListener(this)
-                }
-                override fun onClick(v: View) {
-                        when (v.id) {
-                        R.id.button_signin -> {
-                                        val moveBack = Intent(this@sign_in, Home::class.java)
-                                        startActivity(moveBack)
-                                }
-                                R.id.button_register -> {
-                                        val moveSignUp = Intent(this@sign_in, SignUp::class.java)
-                                        startActivity(moveSignUp)
-                                }
-                        }
-                }
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        Toast.makeText(this ,"firebase connection succes",Toast.LENGTH_LONG).show()
+
+        emailfield = findViewById(R.id.email)
+        passwordfield = findViewById(R.id.password)
+        signinbutton = findViewById(R.id.button_signin)
+        registerbutton = findViewById(R.id.button_register)
+
+        var textUsername = emailfield.text
+        var textPassword = passwordfield.text
+
+        signinbutton.setOnClickListener{
+            Log.e("Email", textUsername.toString())
+            Log.e("Password", textPassword.toString())
         }
+        registerbutton.setOnClickListener(this)
+        }
+        override fun onClick(v: View) {
+            when (v.id) {
+            R.id.button_signin -> {
+                     val moveBack = Intent(this@sign_in, Home::class.java)
+                     startActivity(moveBack)
+                }
+            R.id.button_register -> {
+                     val moveSignUp = Intent(this@sign_in, SignUp::class.java)
+                     startActivity(moveSignUp)
+                }
+            }
+        }
+    }
 
