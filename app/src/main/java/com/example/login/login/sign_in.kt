75a715ca06765070462087f05eaee06b5c585e32
ivander27 <timotius.ivander04@gmail.com>
2022-04-17 16:59:15 +0700

    update sign in

---
+++ app/src/main/java/com/example/login/login/sign_in.kt
@@ -0,0 +1,34 @@
+package com.example.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class sign_in : AppCompatActivity(), View.OnClickListener {
+                override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_sign_in)
+                Toast.makeText(this ,"firebase connection succes",Toast.LENGTH_LONG).show()
+                val button_signin: Button = findViewById(R.id.button_signin)
+                val button_Register: Button = findViewById(R.id.button_register)
+
+                button_signin.setOnClickListener(this)
+                button_Register.setOnClickListener(this)
+                }
+                override fun onClick(v: View) {
+                when (v.id) {
+                R.id.button_signin -> {
+                                val moveBack = Intent(this@sign_in, Home::class.java)
+                                startActivity(moveBack)
+                        }
+                        R.id.button_register -> {
+                                val moveSignUp = Intent(this@sign_in, SignUp::class.java)
+                                startActivity(moveSignUp)
+                        }
+                }
+                }
+        }
+
